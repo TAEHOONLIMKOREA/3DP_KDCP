b3b30e4294cd00b08aea432c64bd0e5694d9199d
Taehoon <lth9029@gmail.com>
2023-04-14 18:39:49 +0900

    test for kafka

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# importing the required libraries
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -14,3 +13,15 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    my_producer = KafkaProducer(
+        bootstrap_servers=['10.0.3.53:55592']
+    )
+
+    # Send messages
+    for i in range(7):
+        message = f"Message {i}"
+        my_producer.send('my_topic', value=message)
+
+    # Close the Kafka producer connection
+    my_producer.close()
