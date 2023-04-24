17b721b71fa50615d5e2e3be5a849cf3636f8020
Taehoon <lth9029@gmail.com>
2023-04-24 22:52:57 +0900

    add KafkaConsumer

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # importing the required libraries
 from time import sleep
 from json import dumps
+import json
 from kafka import KafkaProducer
+from kafka import KafkaConsumer
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -15,13 +17,25 @@ if __name__ == '__main__':
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
     my_producer = KafkaProducer(
-        bootstrap_servers=['10.0.3.53:55592']
+        bootstrap_servers=['keties.iptime.org:55592'],
+        value_serializer=lambda x: dumps(x).encode('utf-8')
     )
 
     # Send messages
-    for i in range(7):
-        message = f"Message {i}"
-        my_producer.send('my_topic', value=message)
+    my_producer.send('msgtest', 'Hello World!!')
 
+    # Kafka broker에 연결합니다.
+    consumer = KafkaConsumer(
+        'msgtest',  # topic 이름
+        bootstrap_servers=['keties.iptime.org:55592'],
+        auto_offset_reset='earliest',  # 가장 처음부터 메시지를 받습니다.
+        enable_auto_commit=True,  # 자동으로 offset을 commit합니다.
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    )
+
+    for message in consumer:
+        print(f"Received message: {message.value}")
+
+    print("End!")
     # Close the Kafka producer connection
     my_producer.close()
